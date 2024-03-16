2d65b3c78533398f3435a584c8953eaec1f06ac3
sputnik <ysy1054917144@outlook.com>
2024-03-16 21:49:48 +0800

    Initial commit

---
+++ MDK-ARM/chassis_task/heap_4.d
@@ -0,0 +1,13 @@
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+chassis_task\heap_4.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdlib.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+chassis_task\heap_4.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stddef.h
+chassis_task\heap_4.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+chassis_task\heap_4.o: ../Inc/FreeRTOSConfig.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+chassis_task\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ MDK-ARM/chassis_task/timers.d
@@ -0,0 +1,15 @@
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/timers.c
+chassis_task\timers.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdlib.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+chassis_task\timers.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stddef.h
+chassis_task\timers.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+chassis_task\timers.o: ../Inc/FreeRTOSConfig.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
+chassis_task\timers.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
